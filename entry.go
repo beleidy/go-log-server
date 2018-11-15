7e747829b0d7c7b7d922ff5f58dbd0670bf88ca5
beleidy <amr@beleidy.uk>
2018-11-15 18:12:29 +0000

    changes entry to id. time & message

--- entry.go
+++ entry.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
 	"time"
 )
 
@@ -10,22 +8,5 @@ import (
 type Entry struct {
 	ID      string    `json:"id"`
 	Time    time.Time `json:"time"`
-	Level   int       `json:"level"`
 	Message string    `json:"message"`
-}
-
-func (e Entry) String() string {
-	return fmt.Sprintf("%s\t%v\t%d\t%s\n", e.ID, e.Time, e.Level, e.Message)
-}
-
-// Entries is an array of Entry structs
-type Entries []Entry
-
-func (es Entries) String() string {
-	var b bytes.Buffer
-	for _, e := range es {
-		b.WriteString(e.String())
-	}
-	return b.String()
-
 }
