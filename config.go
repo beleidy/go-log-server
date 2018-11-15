7b94a2a61856d05d2022cc5afbec886c0e1cc265
beleidy <amr@beleidy.uk>
2018-11-15 17:23:08 +0000

    deletes non-db config

--- config.go
+++ config.go
@@ -2,14 +2,10 @@ package main
 
 // Configuration holds all global configs
 type Configuration struct {
-	LogToScreen      bool
-	LogToFile        bool
-	LogToDb          bool
 	DbHost           string
 	DbPort           uint16
 	DbName           string
 	DbUser           string
 	DbPassword       string
 	DbMaxConnections int
-	LogFilePath      string
 }
